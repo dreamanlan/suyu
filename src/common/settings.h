8d402837fd291f3b3d9b34c00774c148a227029d
JuanCStar <petacas666@hotmail.com>
2024-03-10 09:49:27 +0000

    fix: updated default web api url

--- src/common/settings.h
+++ src/common/settings.h
@@ -612,7 +612,7 @@ struct Values {
 
     // WebService
     Setting<bool> enable_telemetry{linkage, false, "enable_telemetry", Category::WebService};
-    Setting<std::string> web_api_url{linkage, "https://api.suyu.dev", "web_api_url",
+    Setting<std::string> web_api_url{linkage, "https://suyu.dev", "web_api_url",
                                      Category::WebService};
     Setting<std::string> suyu_username{linkage, std::string(), "suyu_username",
                                        Category::WebService};

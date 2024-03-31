ee9a81970b99c34daaed599f9e44c58a16321ab1
dreamanlan <dreaman_163@163.com>
2024-03-31 16:11:15 +0800

    debug build and gen xcode project

--- build_mac_debug.sh
+++ build_mac_debug.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-mkdir build
-cd build
-
 export Qt5_DIR="/opt/homebrew/opt/qt@5/lib/cmake"
 
-cmake .. -GNinja -DCMAKE_BUILD_TYPE=RelWithDebInfo -Dsuyu_USE_BUNDLED_VCPKG=OFF -Dsuyu_TESTS=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_LIBUSB=OFF
+mkdir -p xcodebuild
+cd xcodebuild
+
+cmake .. -GXcode -Dsuyu_USE_BUNDLED_VCPKG=OFF -Dsuyu_TESTS=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_LIBUSB=OFF
+
+cd ..
+mkdir -p build
+cd build
+
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Debug -Dsuyu_USE_BUNDLED_VCPKG=OFF -Dsuyu_TESTS=OFF -DENABLE_WEB_SERVICE=OFF -DENABLE_LIBUSB=OFF
 
 ninja

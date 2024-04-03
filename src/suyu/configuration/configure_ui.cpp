yuzu-emu-mirror/yuzu

Load custom Qt themes from yuzu data directory
- Directory is qt_themes, each theme must be in one folder
    - It should contain a file "style.qss"
    - It may contain an "icons" sub-directory, to override included icons
      (with files like mytheme/icons/colorful/48x48/star.png for example)
    - Directories ending by "_dark" are reserved for dark variant icons.
      They are not listed as themes in the UI.
- If theme directory contains "dark" or "midnight", theme will be considered dark

src/yuzu/configuration/configure_ui.cpp
@@ -106,9 +106,29 @@ ConfigureUi::ConfigureUi(Core::System& system_, QWidget* parent)
 
     InitializeLanguageComboBox();
 
-    for (const auto& theme : UISettings::themes) {
+    for (const auto& theme : UISettings::included_themes) {
         ui->theme_combobox->addItem(QString::fromUtf8(theme.first),
                                     QString::fromUtf8(theme.second));
+    }
+
+    // Add custom styles stored in yuzu directory
+    const QDir local_dir(
+        QString::fromStdString(Common::FS::GetYuzuPathString(Common::FS::YuzuPath::ThemesDir)));
+    for (const QString& theme_dir :
+         local_dir.entryList(QDir::NoDot | QDir::NoDotDot | QDir::Dirs)) {
+        // folders ending with "_dark" are reserved for dark variant icons of other styles
+        if (theme_dir.endsWith(QStringLiteral("_dark"))) {
+            continue;
+        }
+        // Split at _ and capitalize words in name
+        QStringList cased_name;
+        for (QString word : theme_dir.split(QChar::fromLatin1('_'))) {
+            cased_name.append(word.at(0).toUpper() + word.mid(1));
+        }
+        QString theme_name = cased_name.join(QChar::fromLatin1(' '));
+        theme_name += QStringLiteral(" (%1)").arg(tr("Custom"));
+
+        ui->theme_combobox->addItem(theme_name, theme_dir);
     }
 
     InitializeIconSizeComboBox();
@@ -164,7 +184,7 @@ ConfigureUi::~ConfigureUi() = default;
 
 void ConfigureUi::ApplyConfiguration() {
     UISettings::values.theme =
-        ui->theme_combobox->itemData(ui->theme_combobox->currentIndex()).toString().toStdString();
+        ui->theme_combobox->itemData(ui->theme_combobox->currentIndex()).toString();
     UISettings::values.show_add_ons = ui->show_add_ons->isChecked();
     UISettings::values.show_compat = ui->show_compat->isChecked();
     UISettings::values.show_size = ui->show_size->isChecked();
@@ -191,8 +211,7 @@ void ConfigureUi::RequestGameListUpdate() {
 }
 
 void ConfigureUi::SetConfiguration() {
-    ui->theme_combobox->setCurrentIndex(
-        ui->theme_combobox->findData(QString::fromStdString(UISettings::values.theme)));
+    ui->theme_combobox->setCurrentIndex(ui->theme_combobox->findData(UISettings::values.theme));
     ui->language_combobox->setCurrentIndex(ui->language_combobox->findData(
         QString::fromStdString(UISettings::values.language.GetValue())));
     ui->show_add_ons->setChecked(UISettings::values.show_add_ons.GetValue());

dreamanlan/suyu

src/suyu/configuration/configure_ui.cpp
@@ -106,9 +106,29 @@ ConfigureUi::ConfigureUi(Core::System& system_, QWidget* parent)
 
     InitializeLanguageComboBox();
 
-    for (const auto& theme : UISettings::themes) {
+    for (const auto& theme : UISettings::included_themes) {
         ui->theme_combobox->addItem(QString::fromUtf8(theme.first),
                                     QString::fromUtf8(theme.second));
+    }
+
+    // Add custom styles stored in yuzu directory
+    const QDir themes_local_dir(
+        QString::fromStdString(Common::FS::GetSuyuPathString(Common::FS::SuyuPath::ThemesDir)));
+    for (const QString& theme_dir :
+         themes_local_dir.entryList(QDir::NoDot | QDir::NoDotDot | QDir::Dirs)) {
+        // folders ending with "_dark" are reserved for dark variant icons of other styles
+        if (theme_dir.endsWith(QStringLiteral("_dark"))) {
+            continue;
+        }
+        // Split at _ and capitalize words in name
+        QStringList cased_name;
+        for (QString word : theme_dir.split(QChar::fromLatin1('_'))) {
+            cased_name.append(word.at(0).toUpper() + word.mid(1));
+        }
+        QString theme_name = cased_name.join(QChar::fromLatin1(' '));
+        theme_name += QStringLiteral(" (%1)").arg(tr("Custom"));
+
+        ui->theme_combobox->addItem(theme_name, themes_local_dir.filePath(theme_dir));
     }
 
     InitializeIconSizeComboBox();
@@ -164,7 +184,7 @@ ConfigureUi::~ConfigureUi() = default;
 
 void ConfigureUi::ApplyConfiguration() {
     UISettings::values.theme =
-        ui->theme_combobox->itemData(ui->theme_combobox->currentIndex()).toString().toStdString();
+        ui->theme_combobox->itemData(ui->theme_combobox->currentIndex()).toString();
     UISettings::values.show_add_ons = ui->show_add_ons->isChecked();
     UISettings::values.show_compat = ui->show_compat->isChecked();
     UISettings::values.show_size = ui->show_size->isChecked();
@@ -191,8 +211,7 @@ void ConfigureUi::RequestGameListUpdate() {
 }
 
 void ConfigureUi::SetConfiguration() {
-    ui->theme_combobox->setCurrentIndex(
-        ui->theme_combobox->findData(QString::fromStdString(UISettings::values.theme)));
+    ui->theme_combobox->setCurrentIndex(ui->theme_combobox->findData(UISettings::values.theme));
     ui->language_combobox->setCurrentIndex(ui->language_combobox->findData(
         QString::fromStdString(UISettings::values.language.GetValue())));
     ui->show_add_ons->setChecked(UISettings::values.show_add_ons.GetValue());
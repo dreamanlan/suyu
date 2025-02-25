cc09097a3738808429271a3dd8f3e27fabda4390
dreamanlan <dreaman_163@163.com>
2025-02-25 14:20:54 +0800

    Update build.gradle.kts
    fix android build error

--- src/android/build.gradle.kts
+++ src/android/build.gradle.kts
@@ -3,13 +3,13 @@
 
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.1.2" apply false
-    id("com.android.library") version "8.1.2" apply false
+    id("com.android.application") version "8.8.1" apply false
+    id("com.android.library") version "8.8.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
 
 tasks.register("clean").configure {
-    delete(rootProject.buildDir)
+    delete(rootProject.layout.buildDirectory)
 }
 
 buildscript {
@@ -17,6 +17,6 @@ buildscript {
         google()
     }
     dependencies {
-        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.6.0")
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.8.7")
     }
 }

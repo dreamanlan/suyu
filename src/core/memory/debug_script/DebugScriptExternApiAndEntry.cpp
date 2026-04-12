169ce73f9c702aa5ef6b265c1e507ca3fb8fa421
dreamanlan <dreaman_163@163.com>
2026-04-13 00:41:56 +0800

    (debug script) huawei squid capture

--- src/core/memory/debug_script/DebugScriptExternApiAndEntry.cpp
+++ src/core/memory/debug_script/DebugScriptExternApiAndEntry.cpp
@@ -1001,11 +1001,18 @@ std::string DumpMemoryFormattedToString(const void* addr,
     return oss.str();
 }
 
+
+static bool s_DbgScpLoaded = false;
 static bool g_bFrameCapturing = false;
 
+bool IsDbgScpLoaded() {
+    return s_DbgScpLoaded;
+}
 void InitGpuCaptureManager() {
 #if __APPLE__
     GpuCaptureManager::Instance().Init(GpuCaptureBackend::MetalXcode);
+#elif defined(__OHOS__)
+    GpuCaptureManager::Instance().Init(GpuCaptureBackend::HuaweiSquid);
 #else
     GpuCaptureManager::Instance().Init(GpuCaptureBackend::RenderDoc);
 #endif
@@ -1601,6 +1608,7 @@ extern "C" {
 
 void LoadDbgScp(const core::string& log_path, const core::string& load_path)
 {
+    s_DbgScpLoaded = true;
     for (int i = 0; i < c_max_log_file_num; ++i) {
         if (GetLogFilesRef()[i].empty()) {
             auto&& path = Format("%s/dbgscp_log_%d.txt", log_path.c_str(), i);
@@ -1642,6 +1650,7 @@ int DbgScp_Get_Extern(int cmd, int a, double b, const char* c)
 
 void LoadDbgScp(const FString& log_path, const FString& load_path)
 {
+    s_DbgScpLoaded = true;
     for (int i = 0; i < c_max_log_file_num; ++i) {
         if (GetLogFilesRef()[i].empty()) {
             auto&& path = FPaths::Combine(log_path, FString::Printf(TEXT("dbgscp_log_%d.txt"), i));
@@ -1684,6 +1693,7 @@ int DbgScp_Get_Extern(int cmd, int a, double b, const char* c)
 
 void LoadDbgScp(const std::string& log_path, const std::string& load_path)
 {
+    s_DbgScpLoaded = true;
     const int c_path_capacity_max = 1025;
     for (int i = 0; i < c_max_log_file_num; ++i) {
         if (GetLogFilesRef()[i].empty()) {
